410a5c69bdc41c856c2f0fc124ea46005b49bd6e
Ant <antony@springtimesoft.co.nz>
2019-07-20 22:04:19 +1200

    Render results of market page ( not working - page blank )

--- agora/src/pages/MarketPage.js
+++ agora/src/pages/MarketPage.js
@@ -1,8 +1,27 @@
 import React from "react";
 // import { Loading, Tabs, Icon } from "element-react";
+import { API, graphqlOperation } from "aws-amplify";
+import { getMarket} from "../graphql/queries";
 
 class MarketPage extends React.Component {
-  state = {};
+  state = {
+    market: null
+  };
+
+  componentDidMount() {
+    this.handleGetMarket();
+  }
+
+  handleGetMarket = async () => {
+
+    const input = {
+      id: this.props.marketId
+    }
+    const result = await API.graphql(graphqlOperation(getMarket, input));
+    console.log({ result });
+    this.setState({ market: result.data.getMarket });
+
+  }
 
   render() {
     return <div>MarketPage { this.props.marketId }</div>;
